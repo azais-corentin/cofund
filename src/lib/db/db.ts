650fe0825641e3c7f0b59bf8db04401afbef35af
Corentin AZAIS <azaiscorentin@gmail.com>
2025-09-19 20:06:21 +0000

    Use triplit http client on server

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,15 +1,21 @@
 import { browser } from '$app/environment';
-import { TriplitClient } from '@triplit/client';
+import { HttpClient, TriplitClient } from '@triplit/client';
+
+import { PUBLIC_TRIPLIT_SERVER_URL, PUBLIC_TRIPLIT_SERVICE_TOKEN } from '$env/static/public';
 import { schema } from './schema';
 
-export const db = new TriplitClient({
-  storage: browser ? 'indexeddb' : 'memory',
+const sharedConfiguration = {
   schema,
-  serverUrl: 'http://localhost:6543',
-  // This is the service token
-  token:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ4LXRyaXBsaXQtdG9rZW4tdHlwZSI6InNlY3JldCIsIngtdHJpcGxpdC1wcm9qZWN0LWlkIjoibG9jYWwtcHJvamVjdC1pZCJ9.8Z76XXPc9esdlZb2b7NDC7IVajNXKc4eVcPsO7Ve0ug',
-  autoConnect: true,
-});
+  serverUrl: PUBLIC_TRIPLIT_SERVER_URL,
+  token: PUBLIC_TRIPLIT_SERVICE_TOKEN,
+};
+
+export const db = browser
+  ? new TriplitClient({
+      ...sharedConfiguration,
+      storage: 'indexeddb',
+      autoConnect: true,
+    })
+  : new HttpClient(sharedConfiguration);
 
 export const Query = db.query;
