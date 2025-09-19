13ef89d01ff7b90c9d320333a65f1b150b9e6a31
Corentin AZAIS <azaiscorentin@gmail.com>
2025-09-19 22:07:49 +0000

    Simplify db type

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,21 +1,17 @@
 import { browser } from '$app/environment';
-import { HttpClient, TriplitClient, type HttpClientOptions } from '@triplit/client';
+import { HttpClient, TriplitClient } from '@triplit/client';
 
 import { PUBLIC_TRIPLIT_SERVER_URL, PUBLIC_TRIPLIT_SERVICE_TOKEN } from '$env/static/public';
 import { schema } from './schema';
 
-const sharedConfiguration: HttpClientOptions = {
+const sharedConfiguration = {
   schema,
   serverUrl: PUBLIC_TRIPLIT_SERVER_URL,
   token: PUBLIC_TRIPLIT_SERVICE_TOKEN,
 };
 
 export const db = browser
-  ? new TriplitClient({
-      ...sharedConfiguration,
-      storage: 'indexeddb',
-      autoConnect: true,
-    })
+  ? new TriplitClient({ ...sharedConfiguration, storage: 'indexeddb', autoConnect: true })
   : new HttpClient(sharedConfiguration);
 
 export const Query = db.query;
