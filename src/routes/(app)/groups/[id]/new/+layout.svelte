2c9827e4544dec619f2506c46eeeafad60c2b65a
Corentin AZAIS <c.azais@ikalogic.com>
2025-10-30 14:46:15 +0000

    Update new expense breadcrumb

--- src/routes/(app)/groups/[id]/new/+layout.svelte
+++ src/routes/(app)/groups/[id]/new/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+  import { page } from '$app/state';
   import { setBreadcrumb } from '$lib/breadcrumb-state.svelte';
 
   let { children } = $props();
 
-  setBreadcrumb({ name: 'New expense', path: '' });
+  setBreadcrumb({ name: 'New expense', path: `/groups/${page.params.id}/new` });
 </script>
 
 {@render children()}
